cee2b70d0c028a538d716c2e153c60f2d9fe8724
yehan2002 <yehanjaya2002@gmail.com>
2021-07-03 17:21:04 +0530

    Remove invalid test

--- internal/unsafe/asm/stub_test.go
+++ internal/unsafe/asm/stub_test.go
@@ -1,11 +1,10 @@
 package asm
 
-import "testing"
-
-func TestSwapEndian(t *testing.T) {
+/* func TestSwapEndian(t *testing.T) {
 	a := []uint16{0x1112, 0x2122, 0x3132, 0x4142, 0x5152, 0x6162, 0x7172, 0x8182}
 	b := make([]uint16, 8)
 
 	Copy16(a, b)
 	t.Errorf("%#v %#v", a, b)
 }
+*/
